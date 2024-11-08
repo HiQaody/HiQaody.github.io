dc46c601a159de3a485eaf2e0f24cd78ec276d17
HiQaody <hiqaody@gmail.com>
2024-11-08 23:34:39 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
-import { Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../assets/images/Logos/hiqaody.png';
 import Logo1 from '../assets/images/Logos/HiQaody.png';
-import {Link as ScrollLink} from 'react-scroll';
+import { Link as ScrollLink } from 'react-scroll';
 import ScrollToTop from './ScrollToTop';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
@@ -15,36 +15,100 @@ function Header() {
     };
 
     return (
-        <header className="container mx-auto px-16 py-9 lg:px-[10%] md:px-[10%] sm:px-[5%]">
-            <div className="bg-primary py-4 flex justify-between items-center flex-wrap  rounded-[60px]">
+        <header className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-[10%] py-9">
+            <div className="bg-primary py-4 flex justify-between items-center flex-wrap rounded-[60px] relative">
                 {/* Logo */}
-                <div className="flex items-center w-full md:w-auto justify-between md:justify-start">
+                <div className="flex items-center justify-between w-full md:w-auto">
                     <Link to="/" className="mx-auto xl:px-16 sm:px-[10%] md:px-[10%] xl:flex-grow md:flex-grow-0 text-center md:text-left ml-4 xl:ml-0 lg:ml-0">
-                        <img src={Logo} alt="Logo"
-                             className="h-12 w-auto cursor-pointer mx-auto md:mx-0 hidden md:block sm:block"/>
-                        <img src={Logo1} alt="Logo"
-                             className="h-12 w-auto cursor-pointer mx-auto  md:mx-0 block md:hidden sm:hidden"/>
+                        <img
+                            src={Logo}
+                            alt="Logo"
+                            className="h-12 w-auto cursor-pointer mx-auto md:mx-0 hidden md:block sm:block"
+                        />
+                        <img
+                            src={Logo1}
+                            alt="Logo"
+                            className="h-12 w-auto cursor-pointer mx-auto md:mx-0 block md:hidden sm:hidden"
+                        />
                     </Link>
                     <button
                         className="md:hidden text-white focus:outline-none"
                         onClick={toggleMenu}
                     >
-                        {isMenuOpen ? <CloseIcon fontSize="large"/> : <MenuIcon fontSize="large"/>}
+                        {isMenuOpen ? <CloseIcon fontSize="large" /> : <MenuIcon fontSize="large" />}
                     </button>
                 </div>
 
-                {/* Navigation */}
-                <nav
+                {/* Menu Mobile / Tablette (side menu) */}
+                <div
                     className={`${
-                        isMenuOpen ? 'block' : 'hidden'
-                    } md:flex md:space-x-6 w-full md:w-auto md:mx-auto md:justify-start mt-4 md:mt-0 flex flex-col md:flex-row`}
+                        isMenuOpen ? 'translate-x-0' : '-translate-x-full'
+                    } fixed top-0 left-0 h-full w-3/4 bg-primary bg-opacity-90 flex flex-col justify-start items-center p-6 transition-transform duration-300 ease-in-out md:hidden z-20`}
+                >
+                    <div className="flex justify-between w-full items-center">
+                        <button
+                            className="text-white text-2xl"
+                            onClick={toggleMenu}
+                        >
+                            <CloseIcon fontSize="large" />
+                        </button>
+                    </div>
+
+                    <nav className="flex flex-col space-y-6 mt-8">
+                        <ScrollLink
+                            to="category"
+                            smooth={true}
+                            duration={500}
+                            className="text-gray-200 cursor-pointer text-xl"
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            Accueil
+                        </ScrollLink>
+                        <ScrollLink
+                            to="book"
+                            smooth={true}
+                            duration={500}
+                            className="text-gray-200 cursor-pointer text-xl"
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            Projets
+                        </ScrollLink>
+                        <ScrollLink
+                            to="videos"
+                            smooth={true}
+                            duration={500}
+                            className="text-gray-200 cursor-pointer text-xl"
+                            onClick={() => setIsMenuOpen(false)}
+                        >
+                            Blog
+                        </ScrollLink>
+                    </nav>
+
+                    <button
+                        className="btn-color py-3 px-6 rounded-[60px] w-full mt-8"
+                        onClick={() => setIsMenuOpen(false)}
+                    >
+                        <span className="ml-2">Contactez-moi</span>
+                    </button>
+                </div>
+
+                {/* Overlay when the menu is open */}
+                {isMenuOpen && (
+                    <div
+                        className="absolute inset-0 bg-black bg-opacity-50 z-10 md:hidden"
+                        onClick={toggleMenu}
+                    ></div>
+                )}
+
+                {/* Menu Desktop */}
+                <nav
+                    className="hidden md:flex md:space-x-6 w-full md:w-auto md:mx-auto md:justify-start mt-4 md:mt-0 flex-row"
                 >
                     <ScrollLink
                         to="category"
                         smooth={true}
                         duration={500}
                         className="text-gray-200 cursor-pointer py-2 md:py-0 text-center block md:inline"
-                        onClick={() => setIsMenuOpen(false)}
                     >
                         Accueil
                     </ScrollLink>
@@ -53,7 +117,6 @@ function Header() {
                         smooth={true}
                         duration={500}
                         className="text-gray-200 cursor-pointer py-2 md:py-0 text-center block md:inline"
-                        onClick={() => setIsMenuOpen(false)}
                     >
                         Projets
                     </ScrollLink>
@@ -62,28 +125,21 @@ function Header() {
                         smooth={true}
                         duration={500}
                         className="text-gray-200 cursor-pointer py-2 md:py-0 text-center block md:inline"
-                        onClick={() => setIsMenuOpen(false)}
                     >
                         Blog
                     </ScrollLink>
                 </nav>
 
-                {/* Actions (boutons de connexion et d'inscription) */}
-                <div
-                    className={`${
-                        isMenuOpen ? 'block' : 'hidden'
-                    } md:flex space-y-2 md:space-y-0 md:space-x-4 w-full md:w-auto justify-center md:justify-end mt-4 md:mt-0 flex flex-col md:flex-row`}
-                >
-                    <button
-                        className="btn-color py-5 px-4 rounded-[60px] w-full md:w-auto">
+                {/* Actions (Contact Button for Desktop) */}
+                <div className="hidden md:flex space-x-4 justify-center md:justify-end mt-4 md:mt-0">
+                    <button className="btn-color py-3 px-6 rounded-[60px]">
                         <span className="ml-2">Contactez-moi</span>
                     </button>
                 </div>
             </div>
-            <ScrollToTop/>
+            <ScrollToTop />
         </header>
-    )
-        ;
+    );
 }
 
 export default Header;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,40 +1,67 @@
 import React from 'react';
 import MA from "../assets/images/Pub/photo 1.png";
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import {FacebookRounded} from "@mui/icons-material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import PhoneIcon from '@mui/icons-material/Phone';
+import cv from '../assets/images/Decos/cv.png';
 
 function Title() {
     return (
-        <div className="container mx-auto px-16 py-2 lg:px-[10%] md:px-[10%] sm:px-[5%]">
+        <div className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-[10%] py-4">
             <div className="flex flex-col lg:flex-row gap-8">
-                <div className="w-full lg:w-2/3 flex justify-start flex-col ">
+                {/* Text and Description Section */}
+                <div className="w-full lg:w-2/3 flex justify-start flex-col mb-8 lg:mb-0">
                     <span className="font-bold text-[18px] xl:text-[40px] md:text-[30px] sm:text-[20px]">Développeur <br/> Web FullStack</span>
 
-                    <h1 className="font-bold text-[40px] xl:text-[80px] md:text-[60px] sm:text-[60px] py-9">Jacquinot</h1>
+                    <div className="md:flex md:justify-between">
+                        <h1 className="font-bold text-[40px] xl:text-[80px] md:text-[60px] sm:text-[50px] py-5">Jacquinot</h1>
+                        <img
+                            src={cv}
+                            alt="Download CV"
+                            className="block mx-auto mt-4 lg:absolute lg:left-1/2 lg:top-1/3 lg:mt-9 lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2 max-w-[100px] cursor-pointer md:mt-0"
+                        />
+                    </div>
 
                     <div className="flex">
-                        <span className="font-bold bg-amber-500 w-[20px] h-[280px] md:h-[120px] sm:h-[180px] mr-2"></span>
-                        <span className="text-justify">Mes compétences en développement full stack me permettent de jongler entre le front-end et le backend
-                        afin de convertir des idées créatives en solutions solides. Je reste constamment à la pointe des dernières
-                        tendances et je suis prêt à affronter de nouveaux défis et à travailler ensemble sur des projets novateurs.
-                        Créons ensemble quelque chose d'exceptionne</span>
+                        <span className="font-bold bg-amber-500 w-[20px] h-[280px] xl:h-[70px] lg:h-[90px] md:h-[120px] sm:h-[180px] mr-2"></span>
+                        <span className="text-justify text-sm md:text-base">
+                            Mes compétences en développement full stack me permettent de jongler entre le front-end et le backend
+                            afin de convertir des idées créatives en solutions solides. Je reste constamment à la pointe des dernières
+                            tendances et je suis prêt à affronter de nouveaux défis et à travailler ensemble sur des projets novateurs.
+                            Créons ensemble quelque chose d'exceptionnel.
+                        </span>
                     </div>
                 </div>
-                <div className="w-full lg:w-1/3 p-4 flex">
-                    <img src={MA} alt="Jacquinot" className="w-full h-auto max-h-[500px] object-contain"/>
-                    <div>
-                        <li className="flex flex-col space-y-4">
-                            <a href="">
-                                <FacebookRounded className="text-color" fontSize="large"/>
+
+                {/* Image and Social Icons Section */}
+                <div className="w-full lg:w-1/3 p-4 flex relative items-center justify-center">
+                    <img
+                        src={MA}
+                        alt="Jacquinot"
+                        className="w-full h-auto max-h-[400px] object-contain lg:max-h-[500px]"
+                    />
+
+                    {/* Social Icons */}
+                    <div className="flex space-x-4">
+                        <ul className="flex flex-col space-y-4 md:space-y-6 text-[#FFBC00]">
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                <FacebookRoundedIcon fontSize="large" />
                             </a>
-                            <a href="">
-                                <LinkedInIcon className="text-color rounded-3xl" fontSize="large"/>
+                            <a href="mailto:example@example.com" target="_blank" rel="noopener noreferrer">
+                                <EmailOutlinedIcon fontSize="large" />
                             </a>
-                            <a href="">
-                                <InstagramIcon className="text-color rounded-3xl" fontSize="large"/>
+                            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon fontSize="large" />
                             </a>
-                        </li>
+                            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon fontSize="large" />
+                            </a>
+                            <a href="tel:+123456789" target="_blank" rel="noopener noreferrer">
+                                <PhoneIcon fontSize="large" />
+                            </a>
+                        </ul>
                     </div>
                 </div>
             </div>
